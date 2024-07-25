13c4096d0ca05851a8739b3bb24cd55ef983f47a
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-07-25 17:11:31 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+//adding process.env.PORT that enable us that our API can be accessed by any client side code
+//from anywhere or from any machine
+
+const PORT = Process.env.PORT || 8000 ; 
